4850f619c147660c86666f8ce598da00484648b7
Shaik Mohammed Jabir Hussain <118990335+thejabirhussain@users.noreply.github.com>
2026-01-08 23:05:00 +0530

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,18 +14,7 @@ export const Nav = () => {
       className={"fixed top-0 right-0 px-4 py-2 flex items-center h-14 z-50"}
     >
       <div className={"ml-auto flex items-center gap-1"}>
-        <Button
-          onClick={() => {
-            window.open(pkg.homepage, "_blank", "noopener noreferrer");
-          }}
-          variant={"ghost"}
-          className={"ml-auto flex items-center gap-1.5 rounded-full"}
-        >
-          <span>
-            <Github className={"size-4"} />
-          </span>
-          <span>Star on GitHub</span>
-        </Button>
+        
         <Button
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           variant={"ghost"}
